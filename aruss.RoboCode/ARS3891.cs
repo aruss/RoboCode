ea967d9a1c03b26a3faffb44c407a64610d2003b
aruss <arusslabs@gmail.com>
2014-05-08 16:07:44 +0200

    Default setup

---
+++ aruss.RoboCode/ARS3891.cs
@@ -0,0 +1,27 @@
+﻿using Robocode;
+using System.Drawing;
+
+namespace SampleCs
+{
+    public class ARS3891 : AdvancedRobot
+    {
+        public override void Run()
+        {
+            // Black Mamba Mode 
+            SetColors(Color.Black, Color.Black, Color.Black);
+
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+        }
+
+        public override void OnHitByBullet(HitByBulletEvent e)
+        {
+        }
+
+        public override void OnHitRobot(HitRobotEvent e)
+        {
+        }
+    }
+}

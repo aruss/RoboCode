a877a6f9d536c2d7d13edc35fa8b1717f239f337
aruss <arusslabs@gmail.com>
2014-05-08 17:52:29 +0200

    no message

--- RoboCode/ARS3891.cs
+++ RoboCode/ARS3891.cs
@@ -1,7 +1,7 @@
 ﻿using Robocode;
 using System.Drawing;
 
-namespace aruss
+namespace arusslabs
 {
     public class ARS3891 : AdvancedRobot
     {
---
+++ RoboCode/ScanLog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace arusslabs
+{
+	public class ScanLog : Queue<Scan>
+	{
+		private readonly int capacity;
+        private readonly List<Scan> archive;
+
+        public ScanLog(int capacity)
+			: base()
+		{
+			this.capacity = capacity;
+            this.archive = new List<Scan>();
+		}
+
+        public void Add(Scan item)
+		{
+			if (base.Count == this.capacity)
+				archive.Add(base.Dequeue()); 
+
+			base.Enqueue(item);
+		}
+	}
+
+    public class Scan
+	{
+		public double Bearing;
+		public double BearingRadians;
+		public double Distance;
+		public double Energy;
+		public double Heading;
+		public double HeadingRadians;
+		public double Velocity;
+	}
+
+}
